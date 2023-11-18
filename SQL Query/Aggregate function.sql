290deec7fc9c8c715b58b3d48b279119e5781eda
cmrohityadav <cmrohityadav23@gmail.com>
2023-11-18 15:12:26 +0530

    Aggregate function

---
+++ SQL Query/Aggregate function.sql
@@ -0,0 +1,10 @@
+-- Aggregate function perform a calculation on a set of values
+-- and return a single values
+select mark from student;
+select max(mark) from student;
+select min(mark) from student;
+select avg(mark) from student;
+select count(rollno) from student;
+
+
+
