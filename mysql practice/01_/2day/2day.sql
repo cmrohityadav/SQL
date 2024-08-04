18485abfc6124b127d4162a30fe51f5292566a0b
cmrohityadav <cmrohityadav23@gmail.com>
2024-08-04 11:58:24 +0530

    created learners table

--- mysql practice/01_/2day/2day.sql
+++ mysql practice/01_/2day/2day.sql
@@ -30,3 +30,37 @@ INSERT INTO course(courseName,courseDurationMonth,courseFee)
 VALUES("SQL Bootcamp ",1,2999);
 
 SELECT * FROM course;
+
+--creating learner table
+CREATE TABLE learners (
+LearnerId INT AUTO_INCREMENT,
+LearnerFirstName VARCHAR(50) NOT NULL,
+LearnerLastName VARCHAR(50) NOT NULL,
+LearnerPhoneNo VARCHAR(15) NOT NULL,
+LearnerEmailId VARCHAR(50),
+LearnerEnrollmentDate TIMESTAMP NOT NULL,
+SelectedCourses INT NOT NULL,
+YearOfExperience INT NOT NULL,
+LearnerCompany VARCHAR(50) ,
+SourceOfJoining VARCHAR(50) NOT NULL,
+BatchStartDate TIMESTAMP NOT NULL,
+Location VARCHAR(50) NOT NULL,
+PRIMARY KEY(LearnerId),
+UNIQUE KEY(LearnerEmailId),
+FOREIGN KEY (SelectedCourses) REFERENCES course(courseId)
+)
+
+SHOW TABLES
+
+DROP TABLE learners
+
+DESC learners
+
+--Insert the learners details in the learners table
+--Batch start date 1: '2024-02-29'
+-- Batch start date 2: '2024-01-16'
+-- Batch start date 3: '2024-03-25'
+INSERT INTO learners(LearnerFirstName,LearnerLastName,LearnerPhoneNo,LearnerEmailId,LearnerEnrollmentDate,SelectedCourses,YearOfExperience,LearnerCompany,SourceOfJoining,BatchStartDate,Location
+)VALUES ("Akash","Mishra","9874561230","akash@gmail.com",'2024-01-21',1,4,"Amazon","LinkedIn",'2024-02-29',"Bengalure")
+
+SELECT * FROM learners
